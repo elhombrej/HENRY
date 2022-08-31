2ceadbe3c03e7361c333e99b0c7ff3a556c47b5a
Juako <elhombrej@hotmail.com>
2022-08-31 19:56:23 -0300

    31/08

--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -5,10 +5,27 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
     startEl = document.body;
   }
 
+
+
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+
+  if(matchFunc(startEl)){
+
+    resultSet.push(startEl);
+  };
+
+  for (let i = 0; i < startEl.children.length; i++){
+
+    var elements = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+  
+    resultSet = [...resultSet, ...elements];
+
+  }
+
+  return resultSet;
   
 };
 
@@ -19,6 +36,16 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
   
+  if (selector[0] === ".")return "class";
+
+  else if (selector[0] === "#")return "id";
+
+  else if (selector.split(".").length > 1)return "tag.class" 
+  
+  else  
+  
+  return "tag";
+
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,14 +56,44 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
+
   if (selectorType === "id") { 
-   
+
+    matchFunction = function(elemento){
+
+      return `#${elemento.id}` === selector;
+    }
   } else if (selectorType === "class") {
+
+    matchFunction = function(elemento){
+
+      let classes = elemento.classList;
+
+      for (let i = 0; i < classes.length; i++){
+
+        if ("." + classes[i] === selector) return true;
+
+      }
+
+      return false;
+    }
     
   } else if (selectorType === "tag.class") {
     
+    matchFunction = function(elemento){
+
+      let [tagBuscado, classBuscada] = selector.split(".");
+
+      return matchFunctionMaker (tagBuscado)(elemento) && matchFunctionMaker(`.${classBuscada}`)(elemento);
+
+    }
   } else if (selectorType === "tag") {
-    
+
+    matchFunction = function(elemento){
+
+      return elemento.tagName.toLowerCase() === selector;
+//lo devuelve en mayuscula!
+    }
   }
   return matchFunction;
 };
@@ -47,3 +104,4 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+ 
