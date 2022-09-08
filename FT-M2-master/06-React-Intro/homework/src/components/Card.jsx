c8fe314b52423c36987b4111db050267208ddcb5
Juako <elhombrej@hotmail.com>
2022-09-08 20:29:33 -0300

    08/09

--- FT-M2-master/06-React-Intro/homework/src/components/Card.jsx
+++ FT-M2-master/06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max,min,name,img,onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return <div>
+
+<button onClick = {onClose}>onClose</button>
+    <h4>{name}</h4>
+    <div>
+      <p>Min</p>
+      <p>{min}</p>
+      <p>Max</p>
+      <p>{max}</p>
+    </div>
+     <img src = {`http://openweathermap.org/img/wn/${img}@2x.png`} alt ={"img"}></img>
+
+  </div>
 };
--- FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Card from './Card';
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+
+  if(!props.cities) return <h1>No hay ciudades</h1>
+
+  return (<div>{props.cities && props.cities.map(city => (<Card name={city.name} min={city.main.temp_min} max={city.main.temp_max} img={city.weather[0].icon} onClose={()=> alert (city.name)} key={city.id} /> 
+      ))     
+}
+  </div>)
 };
